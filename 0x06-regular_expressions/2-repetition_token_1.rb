e82b9cd7b62c8ef52e15d1470c1a8468dad241bf
Doug-0404 <douglasnyakwara@gmail.com>
2022-10-18 03:13:58 -0700

    take_1000

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,2 +1,4 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/hb{0,1}tn/)
+#a Ruby script that accepts one argument and pass it to a regular expression matching method
+#Find the regular expression that will match any string that contains zero or one occurrences of b
+puts ARGV[0].scan(/hb?tn/).join
--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,2 +1,4 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/^\d{1,10}$/)
+#a Ruby script that accepts one argument and pass it to a regular expression matching method
+#The regular expression must match a 10 digit phone number
+puts ARGV[0].scan(/^\d{10}$/).join
